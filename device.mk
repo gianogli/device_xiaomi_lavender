161f39b5d1b8eb1dae23bf029d74930fc5d917ce
GIanluca <gianogli@tiscali.it>
2021-04-22 00:42:29 +0200

    lavender: overlay: Add LavenderWifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5
    lavender: overlay: Change tethering default SSID

--- device.mk
+++ device.mk
@@ -98,6 +98,10 @@ PRODUCT_PACKAGES += \
 	android.hardware.vibrator@1.0-impl \
 	android.hardware.vibrator@1.0-service
 
+# Wifi
+PRODUCT_PACKAGES += \
+    LavenderWifiOverlay
+
 # Vendor files
 $(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
 
